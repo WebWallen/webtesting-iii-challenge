ccdb7675487116ee9f015be39871cf67ea64a327
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-09-16 16:44:23 -0400

    Display tests done (minus the class one which I'll get help with later).

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,4 +1,3 @@
-// Test away
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import Dashboard from './Dashboard';
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,24 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Display from './Display';
+
+describe('<Display />', () => {
+    it('Shows initial locked and open state upon render', () => {
+        const { getByText } = render(<Display />);
+        expect(getByText('Unlocked'));
+        expect(getByText('Open'));
+    });
+
+    it('Has the expected behavior when props are set a certain way', () => {
+        const { getByText } = render(<Display locked={true} closed={true}/>);
+        expect(getByText('Locked'));
+        expect(getByText('Closed'));
+    });
+
+    it('Sanity test to confirm it behaves when prop settings are reversed', () => {
+        const { getByText } = render(<Display locked={false} closed={false} />);
+        expect(getByText('Unlocked'));
+        expect(getByText('Open'));
+    });
+
+})
