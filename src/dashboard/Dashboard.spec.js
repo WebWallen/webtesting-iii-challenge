2d6cc8ab7ce92c4ebfe85789a4c75e8b88b12e77
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-09-16 15:50:29 -0400

    Dashboard test done.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Dashboard from './Dashboard';
+
+// Write test to confirm it shows controls and display
+
+describe('<Dashboard />', () => {
+    it('Shows default styles upon rendering', () => {
+        const { getByText } = render(<Dashboard />);
+        expect(getByText('Unlocked'));
+        expect(getByText('Lock Gate'));
+        // Ran tests for the rest but guessing user interaction has to happen before they pass
+        // Not worrying about all that stuff since the instruction is merely to see if Display/Controls are there (clearly true)
+    })
+})
